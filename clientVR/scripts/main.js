806397ba4fc6f7b218d4f2284cd279805f1363a8
emiswelt <emiswelt@gmail.com>
2016-04-23 15:13:43 +0200

    split view

--- clientVR/scripts/main.js
+++ clientVR/scripts/main.js
@@ -2,6 +2,7 @@
 var usingDevice = false;
 var camera, scene, renderer;
 var isUserInteracting = false, onMouseDownMouseX = 0, onMouseDownMouseY = 0, lon = 0, onMouseDownLon = 0, lat = 0, onMouseDownLat = 0, phi = 0, theta = 0;
+var effect;
 init();
 animate();
 function init() {
@@ -19,7 +20,10 @@ function init() {
     scene.add(mesh);
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    effect = new THREE.StereoEffect(renderer);
+    //effect = renderer;
+    effect.eyeSeparation = 0;
+    effect.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
     document.addEventListener('mousedown', onDocumentMouseDown, false);
     document.addEventListener('mousemove', onDocumentMouseMove, false);
@@ -38,7 +42,7 @@ function initDeviceOrientation() {
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    effect.setSize(window.innerWidth, window.innerHeight);
 }
 function onDocumentMouseDown(event) {
     event.preventDefault();
@@ -78,9 +82,5 @@ function update() {
         camera.target.z = 500 * Math.sin(phi) * Math.sin(theta);
         camera.lookAt(camera.target);
     }
-    /*
-    // distortion
-    camera.position.copy( camera.target ).negate();
-    */
-    renderer.render(scene, camera);
+    effect.render(scene, camera);
 }
