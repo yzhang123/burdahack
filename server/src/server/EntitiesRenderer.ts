422621eb757ef3b94c9f4b88c407f08b722cab05
Yang Zhang <yang.zhang@cern.ch>
2016-04-24 06:53:11 +0200

    auto fit HTML

--- server/src/server/EntitiesRenderer.ts
+++ server/src/server/EntitiesRenderer.ts
@@ -3,11 +3,19 @@ export function renderEntity(type: string, params: any): string
     var body;
     switch(type)
     {
-        case "bubble.html":
+        case "undefined":
+            body =
+                "";
+            break;
+        case "box":
             body = 
-                "<div style=\"background: linear-gradient(0deg, #aaa, #fff); font-family: sans-serif; font-size: 200px; text-align:center; line-height: 512px; width: 512px; height: 512px; border-radius: 64px;\">" +
+                "<div style=\"background: linear-gradient(0deg, #aaa, #fff); font-family: sans-serif; font-size: 100px; text-align:center; line-height: 256px; width: 512px; height: 256px; border-radius: 64px;\">" +
                 params["text"] +
                 "</div>";
+            break;
+        case "text":
+            body = 
+                params["text"];
             break;
         default:
             body = "<h1>" + type + "</h1>" + JSON.stringify(params);
